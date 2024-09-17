234972c6d6250459a2aafc00be428cc4415a73a8
Macbook Pro <>
2024-09-17 17:13:18 +0700

    done page details pc

--- assets/js/swiper-init.js
+++ assets/js/swiper-init.js
@@ -34,4 +34,15 @@ const swiper = new Swiper('.deera__newkol-swiper', {
   //   slidesPerView: 3,
   //   spaceBetween: 50,
   // });
-  
+
+  // const swiperDetail = new Swiper('.kols__detail-swiper', {
+  //   spaceBetween: 1,
+  //   slidesPerView: 3,
+  //   loop: true,
+  //   centeredSlides: true,
+  //   initialSlide: 1, 
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev"
+  //   }
+  // });
